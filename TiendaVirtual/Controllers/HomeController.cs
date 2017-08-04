248d38d26b0236d3f027179bfbaa4581cd2accb8
JavierSulcaQ <30400372+JavierSulcaQ@users.noreply.github.com>
2017-08-04 11:25:38 -0500

    version 2

--- TiendaVirtual/Controllers/HomeController.cs
+++ TiendaVirtual/Controllers/HomeController.cs
@@ -8,15 +8,23 @@ namespace TiendaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        private Models.TiendaEntities bd = new Models.TiendaEntities();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Buscar()
+        public ActionResult Buscar(string id="")
         {
-            return View();
+            // logica de acceso a base de datos
+            var productos = bd.Producto
+                .Where(x => x.Denominacion.Contains(id))
+                .Take(20)
+                .ToList();
+            //lista con los datos de la busqueda
+            ViewBag.clave = id;
+            return View(productos);
         }
     }
 }
